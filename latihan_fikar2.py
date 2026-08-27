632cf442d20e18598122dd6cf1af0e5778d666c8
julfikar naser <julfikarnaser17@gmail.com>
2026-08-27 21:37:36 +0900

    feat:latihan type data dan konversi input python

---
+++ latihan_fikar2.py
@@ -0,0 +1,7 @@
+# Belajar type data & konversi input di python
+# Input default python itu bertipe teks (string)
+nama_matkul = input("masukan nama matkul: ")
+# Kalau mau olah angka, bungkus input () dengan int()
+sks = int(input("masukan sks:  "))
+print(f"nama matkul {nama_matkul}")
+print(f"sks {sks}")
