b053a61fb6f415b91fad2db7ee12f8c226750cec
julfikar naser <julfikarnaser17@gmail.com>
2026-08-27 21:14:38 +0900

    feat : Latihan dasar variabel python di wsl

---
+++ latihan_fikar1.py
@@ -0,0 +1,5 @@
+#Belajar varriabel dan type data dasar di python
+Nama = "Julfikar Naser"
+Jurusan = "Teknik Informatika"
+Tahun = 2026
+print(f"Nama saya {Nama}, saya kuliah di jurusan {Jurusan},dan saya angkatan {Tahun}")
--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "Halo dari WSL Linux!"
 echo "tanggal hari ini adalah: $(date)"
+#!/bin/bash
+read -p "namamu: " NAMA
+echo "Halo $NAMA, selamat datang di wsl linux!"
